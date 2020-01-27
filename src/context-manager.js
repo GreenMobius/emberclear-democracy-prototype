475b1f99d14b7c23cca29c2f073f7678c4b86926
CelticMajora <cdonoho16@gmail.com>
2020-01-26 21:06:27 -0500

    Fixed delete

--- src/context-manager.js
+++ src/context-manager.js
@@ -102,28 +102,20 @@ class ContextManagerClass {
 
 	delete_user(channel_uid, user_uid, author_uid){
 		var contexts = this.get_all_contexts()
-		var channelContext = contexts.find((context) => context.channel === channel_uid)
-		if(channelContext !== undefined){
-			var authorContext = channelContext.user_contexts.find((userContext) => userContext.user === author_uid)
-			if(authorContext !== undefined){
-				if(authorContext.user_context.members.includes(user_uid)){
-					authorContext.user_context.members = authorContext.user_context.members.filter((member) => member !== user_uid)
-					channelContext.user_contexts = channelContext.user_contexts.filter((userContext) => userContext.user !== user_uid)
-				}
-				this.get_relevant_user_contexts(contexts, channel_uid, author_uid).forEach((userContext) => {
-					if(userContext.user !== authorContext.user){
-						var isAdminMatch = userContext.user_context.admin === authorContext.user_context.admin
-						var membersDiff = userContext.user_context.members
-							.filter(member => !authorContext.user_context.members.includes(member))
-							.concat(authorContext.user_context.members.filter(member => !userContext.user_context.members.includes(member)))
-						console.log("member diff = " + membersDiff)
-						if(isAdminMatch && membersDiff.length === 1 && membersDiff[0] === user_uid && userContext.user_context.members.includes(user_uid)){
-							userContext.user_context = JSON.parse(JSON.stringify(authorContext.user_context))
-							console.log("user: " + userContext.user + " context set to: " + JSON.stringify(userContext.user_context))
-						}
-					}
-				})
+		var authorContext = this.get_user_context_with_contexts_defined(contexts, channel_uid, author_uid)
+		if(authorContext !== undefined){
+			if(authorContext.members.includes(user_uid)){
+				authorContext.members = authorContext.members.filter((member) => member !== user_uid)
 			}
+			this.get_relevant_user_contexts(contexts, channel_uid, author_uid).forEach((userContext) => {
+				var isAdminMatch = userContext.admin === authorContext.admin
+				var membersDiff = userContext.members
+					.filter(member => !authorContext.members.includes(member))
+					.concat(authorContext.members.filter(member => !userContext.members.includes(member)))
+				if(isAdminMatch && membersDiff.length === 1 && membersDiff[0] === user_uid && userContext.members.includes(user_uid)){
+					userContext = JSON.parse(JSON.stringify(authorContext))
+				}
+			})
 		}
 		this.save_all_contexts(contexts)
 	}
