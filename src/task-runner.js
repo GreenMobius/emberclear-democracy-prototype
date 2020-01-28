bd302f1ea4b0a6b5ba9df97af8db4c7d4191c6cd
CelticMajora <cdonoho16@gmail.com>
2020-01-28 14:02:09 -0500

    Had to convert everything to promises. Still wrangling the discord api

--- src/task-runner.js
+++ src/task-runner.js
@@ -12,43 +12,48 @@ class TaskRunnerClass {
     }
 
     addUser(member, role) {
-        member.addRole(role.id);
+        return member.addRole(role.id);
     }
     
     removeUser(member, role) {
-        member.removeRole(role);
+        return member.removeRole(role);
     }
     
     changeAdmin(guild, member, role) {
         const currentAdmin = guild.members.filter(member => member.roles.includes(role))[0];
+        var promises = []
         if(currentAdmin) {
-            currentAdmin.removeRole(adminRole);
+            promises.push(currentAdmin.removeRole(adminRole))
         }
-        member.addRole(adminRole);
+        promises.push(member.addRole(adminRole))
+        return Promise.allSettled(promises)
     }
 
     reset(guild) {
         console.log('clearing roles from ' + guild.name)
+        var promises = []
         guild.roles.forEach((role) => {
-            role.delete().then(deleted => console.log('deleted role ' + deleted.name))
+            promises.push(role.delete().then(deleted => console.log('deleted role ' + deleted.name)))
         })
+        return Promise.allSettled(promises)
     }
 
     setStatus(guild, member, role) {
-        this.reset(guild)
-        this.createChannel(guild, role.name).then(() => {
-            var userContext = contextManager.get_user_context(role.name, member.id)
-            if(userContext !== undefined){
-                const admin = guild.members.find((member) => member.id === userContext.admin)
-                this.changeAdmin(guild, admin, role)
-                userContext.members.forEach((member) => {
-                    let memberObject = guild.members.find((specificMember) => specificMember.id === member)
-                    this.addUser(memberObject, role)
-                })
-                return true
-            }
-            return false
-        })            
+        this.reset(guild).then(() => {
+            this.createChannel(guild, role.name).then(() => {
+                var userContext = contextManager.get_user_context(role.name, member.id)
+                if(userContext !== undefined){
+                    const admin = guild.members.find((member) => member.id === userContext.admin)
+                    this.changeAdmin(guild, admin, role)
+                    userContext.members.forEach((member) => {
+                        let memberObject = guild.members.find((specificMember) => specificMember.id === member)
+                        this.addUser(memberObject, role)
+                    }) 
+                    return true
+                }
+                return false
+            })
+        })
     }
 }
 
